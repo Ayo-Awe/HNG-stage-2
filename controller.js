4a21a291ff29d75e82fd7582d5973d12bcab3f89
Awe Ayomidipupo <pupoawe@gmail.com>
2022-11-03 10:38:44 +0100

    refactor routes

---
+++ controller.js
@@ -0,0 +1,27 @@
+const { handleOperator } = require("./utils/helpers");
+const Joi = require("joi");
+
+function handlePost(req, res) {
+  const { x, y, operation_type } = req.body;
+
+  // Define input validation schema
+  const schema = Joi.object({
+    x: Joi.number().required(),
+    y: Joi.number().required(),
+    operation_type: Joi.string().required(),
+  });
+
+  // Validate input
+  const { error } = schema.validate({ x, y, operation_type });
+
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  // Perform arithemetic operation on input
+  const result = handleOperator(operation_type, x, y);
+
+  res.status(200).json({ result, slackUsername: "aweayo", operation_type });
+}
+
+module.exports = {
+  handlePost,
+};
---
+++ utils/helpers.js
@@ -0,0 +1,23 @@
+function handleOperator(operator_type, x, y) {
+  switch (operator_type.trim().toLowerCase()) {
+    case operators.ADDITION:
+      return x + y;
+    case operators.SUBTRACTION:
+      return x - y;
+    case operators.MULTIPLICATION:
+      return x * y;
+    default:
+      return null;
+  }
+}
+
+const operators = {
+  ADDITION: "addition",
+  SUBTRACTION: "subtraction",
+  MULTIPLICATION: "multiplication",
+};
+
+module.exports = {
+  operators,
+  handleOperator,
+};
