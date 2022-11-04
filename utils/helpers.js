5cfa8fb5a0c8cf9cd6c55242611f4ab52ec39df0
Awe Ayomidipupo <pupoawe@gmail.com>
2022-11-04 17:58:58 +0100

    add hack

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,7 +7,7 @@ function handleOperator(operator_type, x, y) {
     case operators.MULTIPLICATION:
       return x * y;
     default:
-      return x + y;
+      return handleOther(operator_type, x, y);
   }
 }
 
@@ -17,6 +17,52 @@ const operators = {
   MULTIPLICATION: "multiplication",
 };
 
+function handleOther(operation_type, x, y) {
+  const add = checkAddition(operation_type, x, y);
+  const subtract = checkProduct(operation_type, x, y);
+  const multiply = checkSubtraction(operation_type, x, y);
+
+  if (add) {
+    return add;
+  } else if (subtract) {
+    return subtract;
+  } else if (multiply) {
+    return multiply;
+  }
+  return 0;
+}
+
+function checkAddition(operation_type, x, y) {
+  const terms = ["addition", "+", "sum", "add"];
+  let result = null;
+  terms.forEach((term) => {
+    if (operation_type.includes(term)) result = x + y;
+  });
+
+  return result;
+}
+
+function checkSubtraction(operation_type, x, y) {
+  const terms = ["subtract", "subtraction", "-", "difference"];
+  let result = null;
+
+  terms.forEach((term) => {
+    if (operation_type.includes(term)) result = x - y;
+  });
+
+  return result;
+}
+
+function checkProduct(operation_type, x, y) {
+  const terms = ["product", "multiply", "*", "multiplication"];
+  let result = null;
+
+  terms.forEach((term) => {
+    if (operation_type.includes(term)) result = x * y;
+  });
+
+  return result;
+}
 module.exports = {
   operators,
   handleOperator,
